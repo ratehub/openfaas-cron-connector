e3e0f27e335b11821d378df81567a7d6c3fee2eb
Fritz (Fredrick Seitz) <fredrick.seitz@gmail.com>
2021-06-22 13:28:47 -0400

    fix: add nr transactions

--- src/CronJob.js
+++ src/CronJob.js
@@ -1,3 +1,4 @@
+const newrelic = require('newrelic');
 const cron = require("node-cron");
 const fetch = require("node-fetch");
 
@@ -32,28 +33,27 @@ class CronJob {
 
     //Function to be called by cron scheduler
     async executeJob(faasURI) {
-        try {
-            //Invoke function
-            let functionResponse = await fetch(`${faasURI}/async-function/${this.functionName}`, {
-                method: 'post',
-                timeout: this.requestTimeout
-            });
-            if (functionResponse.ok) {
-                this.logger.info(`Successfully invoked function: ${this.functionName}`);
+        await newrelic.startBackgroundTransaction(`trigger-${this.functionName}`, async () => {
+            try {
+                //Invoke function
+                let functionResponse = await fetch(`${faasURI}/async-function/${this.functionName}`, {
+                    method: 'post',
+                    timeout: this.requestTimeout
+                });
+                if (functionResponse.ok) {
+                    this.logger.info(`Successfully invoked function: ${this.functionName}`);
+                } else {
+                    this.logger.error(`Error invoking function: ${this.functionName}`);
+                    this.logger.error(`Status: ${functionResponse.statusText}`);
+                    throw Error(await functionResponse.text());
+                }
+            } catch (err) {
+                this.logger.info(`${err}`);
+                throw err;
+            } finally {
+                this.logger.info(`Finished: ${this.functionName}`);
             }
-            else {
-                this.logger.error(`Error invoking function: ${this.functionName}`);
-                this.logger.error(`Status: ${functionResponse.statusText}`);
-                throw Error(await functionResponse.text());
-            }
-        }
-        catch (err) {
-            this.logger.info(`${err}`);
-            throw err;
-        }
-        finally {
-            this.logger.info(`Finished: ${this.functionName}`);
-        }
+        });
     }
 
     //Destroy running cron job permanently
